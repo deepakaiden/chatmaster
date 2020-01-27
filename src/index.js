4e428212ac13467ba3c56db31c004c49e47c9797
deepakaiden <51381811+deepakaiden@users.noreply.github.com>
2020-01-27 17:34:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,6 +22,15 @@ firebase.initializeApp({
     measurementId: "G-LCPV8W8JKR"
 });
 
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+};
+
 const routing = (
     <Router>
         <div id= 'routing-container'>
